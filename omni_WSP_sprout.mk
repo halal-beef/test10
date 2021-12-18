98ad23a890772d4a4a57eb43ab4b3ab7dffc93a3
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-12-18 17:43:40 +0000

    Update omni_WSP_sprout.mk

--- omni_WSP_sprout.mk
+++ omni_WSP_sprout.mk
@@ -18,6 +18,7 @@
 PRODUCT_RELEASE_NAME := Nokia 2.2
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
